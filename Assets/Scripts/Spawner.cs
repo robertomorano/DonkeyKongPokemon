8614e077829af5b2f9c167869794f7aa0558d236
robertomorano <roberto.morano@iesnervion.es>
2025-12-05 12:56:16 +0100

    animation

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,19 +3,29 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
+    public Animator animator;
     public GameObject barrel;
     public float minTime = 2.0f;
     public float maxTime = 4.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = GetComponent<Animator>();
         Spawn();
     }
 
-
     private void Spawn()
     {
+        // 1. Instanciar el objeto
         Instantiate(barrel, transform.position, Quaternion.identity);
+
+        // 2. Disparar el Trigger (la animación se ejecuta y se resetea automáticamente)
+        if (animator != null)
+        {
+            animator.SetTrigger("Launch"); // Usa el nombre de tu Trigger aquí
+        }
+
+        // 3. Re-invocar para la siguiente aparición
         Invoke(nameof(Spawn), Random.Range(minTime, maxTime));
     }
     
